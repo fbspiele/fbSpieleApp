e5ae31f45dd8ee6a7ca2a9916690a6276fcdb53c
buselmu <buselmu@gmail.com>
2022-02-20 19:01:37 +0100

    cleaned up build gradle

--- app/src/main/java/com/fbspiele/fbspieleapp/MainActivity.java
+++ app/src/main/java/com/fbspiele/fbspieleapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.fbspiele.fbspieleapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,24 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
     Handler handler;
 
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.v(tag,"onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().add(R.id.activity_main_frame_layout, new allGamesFragment(fragmentManager)).commit();
 
         handler = new Handler();
 
-        LinearLayout rootLinearView = findViewById(R.id.activity_main_child_linear_view_id);
+        LinearLayout rootLinearViewAll = findViewById(R.id.activity_main_linear_view_all);
+        //LinearLayout rootLinearViewGames = findViewById(R.id.fragment_games_linear_layout);
 
         connectivity = new Connectivity(this);
-        connectionCard = setUpConnectionCard(rootLinearView);
-        setUpSettingsCard(rootLinearView);
-        setUpBuzzerCard(rootLinearView);
-        setUpWoLiegtWasCard(rootLinearView);
-        setUpBierathlonCard(rootLinearView);
-        setUpSchaetztnCard(rootLinearView);
+        connectionCard = setUpConnectionCard(rootLinearViewAll);
+        //setUpBuzzerCard(rootLinearViewGames);
+        //setUpWoLiegtWasCard(rootLinearViewGames);
+        //setUpBierathlonCard(rootLinearViewGames);
+        //setUpSchaetztnCard(rootLinearViewGames);
     }
 
     @Override
@@ -62,60 +73,81 @@ public class MainActivity extends AppCompatActivity {
         connectionStatus.setText(statusMessage);
     }
 
-    CardView setUpCardStandardCard(LinearLayout rootLinearView, String titleText, int backgroundColor){
-        View cardLayout = View.inflate(this,R.layout.card_layout,null);
-        CardView card = cardLayout.findViewById(R.id.card_view);
-        card.setCardBackgroundColor(backgroundColor);
-        TextView title = card.findViewById(R.id.textViewTitle);
-        title.setText(titleText);
-        rootLinearView.addView(cardLayout);
-        return card;
+/*
     }
 
-    void setUpSchaetztnCard(LinearLayout rootLinearView){
-        CardView card = setUpCardStandardCard(rootLinearView,
-                getString(R.string.maincards_schaetztn_title_text),
-                getResources().getColor(R.color.blue_100));
-        card.setOnClickListener(view -> onSchaetztnCardClick());
+*/
+    public static class allGamesFragment extends Fragment {
+        FragmentManager fragmentManager;
+
+        public allGamesFragment(FragmentManager fragmentManager){
+            this.fragmentManager = fragmentManager;
+        }
+        View fragmentView;
+        Context context;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            fragmentView = inflater.inflate(R.layout.fragment_games, container, false);
+            context = getContext();
+            return fragmentView;
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            LinearLayout linearLayout = (LinearLayout) view;
+
+            View cardSettings = getStandardCard(context, linearLayout, getString(R.string.maincards_settings_title_text), getResources().getColor(R.color.gray_300));
+            cardSettings.findViewById(R.id.card_view).setOnClickListener(view1 -> onSettingsCardClick());
+
+            View cardBuzzer = getStandardCard(context, linearLayout, getString(R.string.maincards_buzzer_title_text), getResources().getColor(R.color.deep_orange_200));
+            cardBuzzer.setOnClickListener(view1 -> onBuzzerCardClick());
+
+            View cardBierathlon = getStandardCard(context, linearLayout, getString(R.string.maincards_bierathlon_title_text), getResources().getColor(R.color.amber_200));
+            cardBierathlon.setOnClickListener(view1 -> onBierathlonCardClick());
+
+            View cardWoLiegtWas = getStandardCard(context, linearLayout,getString(R.string.maincards_woLiegtWas_title_text),getResources().getColor(R.color.teal_200));
+            cardWoLiegtWas.setOnClickListener(view1 -> onWoLiegtWasCardClick());
+
+
+            View getSchaetztnCard = getStandardCard(context, linearLayout,getString(R.string.maincards_schaetztn_title_text),getResources().getColor(R.color.blue_100));
+            getSchaetztnCard.setOnClickListener(view1 -> onSchaetztnCardClick());
+        }
+
+        void onBierathlonCardClick(){
+            //todo
+            fragmentManager.beginTransaction().remove(this).commit();
+        }
+
+        void onWoLiegtWasCardClick(){
+            //todo
+        }
+
+
+        void onBuzzerCardClick(){
+            //todo
+            //findViewById(R.id.activity_main_linear_view_games).setBackgroundColor(getResources().getColor(R.color.teal_200));
+        }
+
+        void onSettingsCardClick(){
+            startActivity(new Intent(context, SettingsActivity.class));
+        }
+
+        void onSchaetztnCardClick(){
+            //todo
+        }
+
+        View getStandardCard(Context context, ViewGroup viewGroup, String titleText, int backgroundColor){
+            View inflated = LayoutInflater.from(context).inflate(R.layout.card_layout, viewGroup, false);
+            CardView card = inflated.findViewById(R.id.card_view);
+            card.setCardBackgroundColor(backgroundColor);
+            TextView title = card.findViewById(R.id.textViewTitle);
+            title.setText(titleText);
+            viewGroup.addView(inflated);        // add inflated but return card!
+            return card;
+        }
     }
 
-    void onSchaetztnCardClick(){
-        //todo
-    }
-
-
-    void setUpBierathlonCard(LinearLayout rootLinearView){
-        CardView card = setUpCardStandardCard(rootLinearView,
-                getString(R.string.maincards_bierathlon_title_text),
-                getResources().getColor(R.color.amber_200));
-        card.setOnClickListener(view -> onBierathlonCardClick());
-    }
-
-    void onBierathlonCardClick(){
-        //todo
-    }
-
-    void setUpWoLiegtWasCard(LinearLayout rootLinearView){
-        CardView card = setUpCardStandardCard(rootLinearView,
-                getString(R.string.maincards_woLiegtWas_title_text),
-                getResources().getColor(R.color.teal_200));
-        card.setOnClickListener(view -> onWoLiegtWasCardClick());
-    }
-
-    void onWoLiegtWasCardClick(){
-        //todo
-    }
-
-    void setUpBuzzerCard(LinearLayout rootLinearView){
-        CardView card = setUpCardStandardCard(rootLinearView,
-                getString(R.string.maincards_buzzer_title_text),
-                getResources().getColor(R.color.deep_orange_200));
-        card.setOnClickListener(view -> onBuzzerCardClick());
-    }
-
-    void onBuzzerCardClick(){
-        //todo
-    }
 
     CardView setUpConnectionCard(LinearLayout rootLinearView){
         View cardLayout = View.inflate(this,R.layout.card_layout,null);
@@ -125,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
         TextView settingsTitle = connectionCard.findViewById(R.id.textViewTitle);
         settingsTitle.setText(getString(R.string.maincards_connection_title_text));
         connectionCard.setOnClickListener(view -> onConnectionCardClick());
-        rootLinearView.addView(cardLayout);
+        rootLinearView.addView(cardLayout,0);
 
         TextView statusTextView = new TextView(this);
         statusTextView.setText(getString(R.string.maincards_connection_disconnected_text));
@@ -135,7 +167,6 @@ public class MainActivity extends AppCompatActivity {
 
         return connectionCard;
     }
-
     void onConnectionCardClick(){
         if(connected){
             pingServer();
@@ -186,15 +217,4 @@ public class MainActivity extends AppCompatActivity {
         // just connectivity.fbsendtosocket(message); leads to android.os.NetworkOnMainThreadException so detour over new thread even though it seems to work in win10control
         new Thread(() -> connectivity.fbsendtosocket(message)).start();
     }
-
-
-
-
-    void setUpSettingsCard(LinearLayout rootLinearView){
-        CardView card = setUpCardStandardCard(rootLinearView,
-                getString(R.string.maincards_settings_title_text),
-                getResources().getColor(R.color.gray_300));
-        card.setOnClickListener(view -> startActivity(new Intent(this, SettingsActivity.class)));
-    }
-
 }
